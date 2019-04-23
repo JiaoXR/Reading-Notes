fb93e946fb7c88631c411c332d03d257c0ea717f
jiaoxiangru <jiaoxiangru@gmail.com>
2019-04-24 00:32:27 +0800

    add pic

---
+++ codes/TestCode/src/com/jaxer/example/jvm/reference/SubClass.java
@@ -0,0 +1,7 @@
+package com.jaxer.example.jvm.reference;
+
+/**
+ * Created by jaxer on 2019-04-23
+ */
+public class SubClass {
+}
---
+++ codes/TestCode/src/com/jaxer/example/jvm/reference/SuperClass.java
@@ -0,0 +1,9 @@
+package com.jaxer.example.jvm.reference;
+
+/**
+ * 主动引用与被动引用
+ * <p>
+ * Created by jaxer on 2019-04-23
+ */
+public class SuperClass {
+}
